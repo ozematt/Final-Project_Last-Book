dcacd3667f7ce9e0ef0296c292094904a489a8a0
Mateusz Obirek <neutralnie7@gmail.com>
2024-06-07 11:12:52 +0200

    vite config file improved

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,10 @@
+// vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-/**
- * Path to exercise folder
- */
-const exercisePath = "Last-Book";
-
-/**
- * Don't change those lines below
- */
 export default defineConfig({
-  root: exercisePath,
+  plugins: [react()],
   server: {
     port: 3001,
   },
-  plugins: [react()],
 });
